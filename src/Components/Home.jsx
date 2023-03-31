0b7a473d5d030cc57689351784b7d54f037e7355
JHANANI27 <108513771+JHANANI27@users.noreply.github.com>
2023-03-31 15:13:00 +0530

    Add files via upload

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Components/home.css"
+export default function Home() {
+  return (
+    <div>
+        <div className='div-main'>
+           <h1>More Esperesso,
+           </h1><br/>
+               <h2>Less Depresso</h2>
+            <p>Coffee all Days and Nights</p><br/><br/>
+            <a href=''>Read More</a>
+        </div>
+    </div>
+  )
+}
